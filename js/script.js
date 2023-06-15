0309027e10e51c10a3e6b026494713b29e5a0817
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2023-06-15 04:20:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -87,3 +87,58 @@ let slideIndex = 1,
 		}
 	});
 
+
+
+
+
+
+
+
+
+let tab = document.querySelectorAll(".info-header-tab"),
+	info = document.querySelector(".info-header"),
+	tabContect = document.querySelectorAll(".info-tabcontent");
+function hideTabContect(a) {
+	for (let i = a; i < tabContect.length; i++) {
+		tabContect[i].classList.remove("show");
+		tabContect[i].classList.add("hide");
+	}
+}
+hideTabContect(1);
+function ShowTabContect(b) {
+	if (tabContect[b].classList.contains("hide")) {
+		tabContect[b].classList.remove("hide");
+		tabContect[b].classList.add("show");
+	}
+}
+
+
+info.addEventListener("click", function (event) {
+	let target = event.target;
+	if (target && target.classList.contains("info-header-tab")) {
+		for (let i = 0; i < tab.length; i++) {
+			if (target == tab[4]) {
+				hideTabContect(0);
+				tab.forEach((item) => {
+						item.classList.remove("active");
+				});
+				target.classList.add("active");
+
+				for (let i = 0; i < 4; i++) {
+						ShowTabContect(i);
+				}
+				break;
+			}
+			else if (target == tab[i]) {
+				hideTabContect(0);
+				tab.forEach((item) => {
+						item.classList.remove("active");
+				});
+				target.classList.add("active");
+				ShowTabContect(i);
+
+				break;
+			}
+		}
+	}
+});
